f87529927b034419980a63804bcaf126101f4f13
Delfina <delfinamassango2@gmail.com>
2025-11-13 20:43:44 +0200

    added a new fragment for favorites

---
+++ app/src/main/java/com/example/opsc6312finalpoe/fragments/FavoritesFragment.kt
@@ -0,0 +1,25 @@
+package com.example.opsc6312finalpoe.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.opsc6312finalpoe.databinding.FragmentFavoritesBinding
+
+class FavoritesFragment : Fragment() {
+    private lateinit var binding: FragmentFavoritesBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Initialize favorites content
+    }
+}
